87a57e0ee40ae11287b473891c23701dd90c78b0
Sohan Rai <sohanr@juniper.net>
2022-05-10 10:15:37 +0530

    First

---
+++ int_status.py
@@ -0,0 +1,31 @@
+# Script to fetch the Admin and Operational status of an Interface using PyEz.
+# Script uses RPC instead of configuration scrapping.
+
+
+from jnpr.junos import Device
+
+
+dev = Device(host='x.x.x.x',user='user',password='password')
+dev.open()
+
+
+# List of interfaces to check
+intf_list = ['xe-1/0/0','et-0/2/0','ge-0/0/0']
+
+
+# Function to fetch the Admin and Operational status of the Interface
+def intf_status_check(intf):
+    rpc_result = dev.rpc.get_interface_information(terse=True, interface_name=intf)
+    admin_status = rpc_result.find("./physical-interface/admin-status")
+    oper_status = rpc_result.find("./physical-interface/oper-status")
+    return admin_status.text.strip(), oper_status.text.strip()
+
+
+for interface in intf_list:
+    admin, oper = intf_status_check(interface)
+    print("*" * 20)
+    print(interface, admin, oper)
+
+print("*" * 20)
+
+dev.close()
